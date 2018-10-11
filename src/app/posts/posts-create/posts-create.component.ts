4f2b29126e77fd2f073eb7c012b35efd1a4b0a1d
Sarfaraz Hussain <sarfarazsajjad@gmail.com>
2018-10-11 10:26:17 +0500

    mat-error

--- src/app/posts/posts-create/posts-create.component.ts
+++ src/app/posts/posts-create/posts-create.component.ts
@@ -1,25 +1,27 @@
-import { Component,Output,EventEmitter } from "@angular/core";
+import { Component, Output, EventEmitter } from "@angular/core";
 import { NgForm } from '@angular/forms'
-import {Post} from "../post.model"
+import { Post } from "../post.model"
 @Component({
-  selector:"app-posts-create",
-  templateUrl:"./posts-create.component.html",
-  styleUrls:["./posts-create.component.css"]
+  selector: "app-posts-create",
+  templateUrl: "./posts-create.component.html",
+  styleUrls: ["./posts-create.component.css"]
 })
-export class PostsCreateComponent{
+export class PostsCreateComponent {
 
   enteredTitle = '';
   enteredContent = '';
 
   @Output() createdPost = new EventEmitter;
 
-  onSavePost(form : NgForm){
-    const post: Post = {
-      title: form.value.title,
-      content: form.value.content
-    }
+  onSavePost(form: NgForm) {
 
-    this.createdPost.emit(post);
+    if (form.valid) {
+      const post: Post = {
+        title: form.value.title,
+        content: form.value.content
+      }
+      this.createdPost.emit(post);
+    }
 
   }
 }
